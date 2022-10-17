8413c593a0be32601aa7f269ea3983e7b1263b10
Edson0012 <Edsonewertonrs1@gmail.com>
2022-10-16 23:58:12 -0300

    terminada função de deletar URls

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,5 +1,4 @@
 import { connection } from "../database/database.js";
-import jwt from "jsonwebtoken";
 import { nanoid } from "nanoid";
 import isUrl from "is-url";
 
@@ -39,5 +38,53 @@ export async function sendUrlShortener (req, res){
     }catch(err){
         console.log(err);
         return res.status(500).send('server error')
+    }
+}
+
+export async function redirectUser(req, res){
+    const { shortUrl } = req.params;
+
+    try{
+
+        const{rows: userUrl}  = await connection.query(`SELECT links.url, links."visitCount" FROM links WHERE "shortUrl" = $1;`, [shortUrl])
+
+        if(!userUrl[0].url){
+            return res.status(404);
+        }
+        
+         await connection.query(`UPDATE links SET "visitCount" = "visitCount" + 1 WHERE "shortUrl" = $1;`, [shortUrl])
+
+        return res.redirect(userUrl[0].url)
+
+    }catch(err){
+        console.log(err);
+        return res.status(500).send('server error');
+    }
+}
+
+export async function deleteUrl (req, res){
+    const { id } = req.params;
+    const { user } = res.locals;
+
+    try{
+
+        const {rows: selectId} = await connection.query(`
+        SELECT * FROM links JOIN users ON users.id = links."usersId" WHERE links.id = $1;`
+        , [id])
+
+        if(user.email !== selectId[0].email){
+            return res.sendStatus(401)
+        }
+
+        if(!selectId[0].shortUrl){
+            return res.sendStatus(404);
+        }
+        
+        await connection.query(`DELETE FROM links WHERE id = $1;`, [id]) 
+
+        return res.sendStatus(204);
+    }catch(err){
+        console.log(err);
+        return res.status(500).send('server error');
     }
 }
--- src/routers/urlRouter.js
+++ src/routers/urlRouter.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { sendUrlShortener, shortenerUrl } from "../controllers/urlController.js";
+import { deleteUrl, redirectUser, sendUrlShortener, shortenerUrl } from "../controllers/urlController.js";
 import { validateToken } from "../middlewares/tokenValidate.js";
 import { validateSchema } from "../middlewares/validate.js";
 import { urlSchema } from "../schemas/urlSchema.js";
@@ -7,6 +7,8 @@ import { urlSchema } from "../schemas/urlSchema.js";
 const router = Router();
 
 router.post('/urls/shorten', validateToken, validateSchema(urlSchema), shortenerUrl);
-router.get('/urls/:id', validateToken, sendUrlShortener)
+router.get('/urls/:id', sendUrlShortener);
+router.get('/urls/open/:shortUrl', redirectUser);
+router.delete('/urls/:id', validateToken, deleteUrl)
 
 export default router
